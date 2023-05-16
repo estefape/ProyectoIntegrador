bf267b417dab7bb52c8aa027e8e6ed2e8291e876
Estefania Mingochea <estefape06@gmail.com>
2023-05-16 15:01:48 -0300

    Arreglar tamaño de imagenes, renombrar categorias y background recomendados.

--- frontend/src/components/cardCategory/CardCategory.jsx
+++ frontend/src/components/cardCategory/CardCategory.jsx
@@ -1,6 +1,6 @@
 import "./cardCategory.css";
 
-export const CardCategory = ({image}) => {
+export const CardCategory = ({image, name}) => {
   // cardCategory va a recibir cada objeto Category del back
   // y vamos a mostrar una imagen representativa de la categoria
   // el nombre de la categoria y la cantidad de resultados para esa categoria aprox
@@ -14,7 +14,7 @@ export const CardCategory = ({image}) => {
         />
         <div className="card-category-body">
           {/* categoria */}
-          <h4>Categoria</h4>
+          <h4>{name}</h4>
           {/* cantidad de resultados */}
           <p>Cantidad de resultados</p>
         </div>
