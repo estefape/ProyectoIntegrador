bb8d7fa5339c9427a59a3c651389fb6d509517b2
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-23 22:04:06 -0500

    se agrego el comando terraform apply al CI/CD y se modifico el main.tf para crear el bucket del frontend

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,3 +5,24 @@ terraform {
     region = "us-east-2"
   }
 }
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-c3-equipo3"
+
+  # Public access
+  acl    = "public-read"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::frontend-c3-equipo3/*"
+    }
+  ]
+}
+EOF
+}
