3e9a69478cd4d581a7c2d8cdf4d72943fbe4f4ee
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-24 15:35:22 -0500

    se modificó el main.tf en la linea 40

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -37,6 +37,7 @@ resource "aws_eip" "elastic_ip" {
 }
 
 resource "aws_instance" "backend_instance" {
+  key_name = "backend-c3-equipo3"
   ami           = "ami-0430580de6244e02e" // Ubuntu Server 20.04 LTS
   instance_type = "t2.micro"
   subnet_id     = "subnet-07388b09d5835ee84"
