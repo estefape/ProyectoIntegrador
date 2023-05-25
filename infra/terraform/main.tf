ca4bea54805d64759985e30a2a63688be520c4db
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-25 13:05:40 -0500

    se agregó en main.tf el recurso para crear un security group

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,6 +6,7 @@ terraform {
   }
 }
 
+# ..... S3 ..... #
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = "frontend-c3-equipo3"
 
@@ -47,6 +48,7 @@ data "aws_vpc" "default_vpc" {
   default = true
 }
 
+# ..... IP ..... #
 resource "aws_eip" "elastic_ip" {
   vpc   = true
 
@@ -56,9 +58,10 @@ resource "aws_eip" "elastic_ip" {
   }
 }
 
+# ..... EC2 ..... #
 resource "aws_instance" "backend_instance" {
   ami           = "ami-0430580de6244e02e" // Ubuntu Server 20.04 LTS
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   subnet_id     = "subnet-07388b09d5835ee84"
 
   tags  = {
@@ -72,3 +75,45 @@ resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.backend_instance.id
   allocation_id = aws_eip.elastic_ip.id
 }
+
+# ..... Security group ..... #
+resource "aws_security_group" "backend" {
+  name   = "security-group-equipo3"
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ..... Keypair ..... #
+/*resource "aws_key_pair" "keypair_instance_equipo3" {
+  key_name = "key_equipo3"
+  public_key = "tls_private_key.rsa.public_key_openssh"
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "local_file" "keypair_file_equipo3" {
+  filename = "key_equipo3"
+  content = tls_private_key.rsa.private_key_pem
+}
+/
