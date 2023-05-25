21bae1e625ca0495ff69e27b3b29878db62662e9
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-24 21:05:13 -0500

    se agrego en main.tf la policy del bucket

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -24,6 +24,25 @@ resource "aws_s3_bucket_public_access_block" "frontend_public_access" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.id}/*"
+    }
+  ]
+}
+EOF
+}
+
 data "aws_vpc" "default_vpc" {
   default = true
 }
