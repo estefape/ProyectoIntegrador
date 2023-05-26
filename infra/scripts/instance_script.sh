c308474fdd4872d61edf41efd6cb993dea2f9abb
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-25 19:51:47 -0500

    se modifico el script para instalar Pack

--- infra/scripts/instance_script.sh
+++ infra/scripts/instance_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Verificar máquina virtual
+echo "Host: $(hostname)"
+echo "OS: $(uname -a)"
+
 # Actualizar el sistema
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -8,12 +12,3 @@ sudo apt-get upgrade -y
 sudo add-apt-repository ppa:cncf-buildpacks/pack-cli
 sudo apt-get update
 sudo apt-get install pack-cli
-
-# Instalar Docker
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
