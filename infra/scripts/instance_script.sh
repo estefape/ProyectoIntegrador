98f87830d315b03483863dcc45f908a75e0954c4
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-25 20:12:22 -0500

    se modifico el script de instalacion

--- infra/scripts/instance_script.sh
+++ infra/scripts/instance_script.sh
@@ -9,6 +9,31 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Instalar Pack
-sudo add-apt-repository ppa:cncf-buildpacks/pack-cli
-sudo apt-get update
-sudo apt-get install pack-cli
+if ! command -v pack &>/dev/null; then
+  echo "Instalación de Pack"
+  sudo add-apt-repository ppa:cncf-buildpacks/pack-cli
+  sudo apt-get update
+  sudo apt-get install pack-cli
+else
+  echo "Pack ya está instalado"
+fi
+
+echo "La versión de Pack instalada es:"
+pack --version
+
+# Instalar Docker
+if ! command -v docker &>/dev/null; then
+  echo "Instalación de Docker"
+  sudo apt update
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install docker-ce
+else
+  echo "Docker ya está instalado"
+fi
+
+echo "La versión de Docker instalada es:"
+docker --version
