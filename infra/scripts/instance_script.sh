0faae5ef70fbb13384df19298f1c97fe4dcd09c6
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-28 15:55:43 -0500

    se modifico el script

--- infra/scripts/instance_script.sh
+++ infra/scripts/instance_script.sh
@@ -4,7 +4,7 @@ ruta="/home/ubuntu"
 nombre_usuario="cinthyarondon"
 nombre_repositorio="digitalbooking"
 etiqueta="latest"
-container_id=$(docker ps --filter "publish=80" --format "{{.ID}}")
+container_id=$(sudo docker ps --filter "publish=80" --format "{{.ID}}")
 
 # Verificar máquina virtual
 echo "Host: $(hostname)"
@@ -34,10 +34,10 @@ if [ -n "$container_id" ]; then
   echo "Se encontró un contenedor en ejecución con el puerto 80 asignado. Deteniendo y eliminando el contenedor..."
 
   # Detener el contenedor
-  docker stop "$container_id"
+  sudo docker stop "$container_id"
 
   # Eliminar el contenedor
-  docker rm "$container_id"
+  sudo docker rm "$container_id"
 
   echo "El contenedor ha sido detenido y eliminado correctamente."
 else
@@ -52,14 +52,14 @@ if [ -f "$ruta/Dockerfile" ]; then
     echo "La carpeta 'backend' existe en la ruta especificada."
 
     # Construir la imagen
-    docker build -t "$nombre_usuario/$nombre_repositorio:$etiqueta" .
+    sudo docker build -t "$nombre_usuario/$nombre_repositorio:$etiqueta" .
 
     # Verificar si la construcción de la imagen fue exitosa
     if [ $? -eq 0 ]; then
       echo "La imagen se ha construido correctamente."
 
       # Ejecutar el contenedor
-      docker run -d -p 80:8080 "$nombre_usuario/$nombre_repositorio:$etiqueta"
+      sudo docker run -d -p 80:8080 "$nombre_usuario/$nombre_repositorio:$etiqueta"
     else
       echo "La construcción de la imagen ha fallado."
     fi
