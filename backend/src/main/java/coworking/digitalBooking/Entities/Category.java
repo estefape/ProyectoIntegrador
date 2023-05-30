ebe2cb84f3e62c852418ff3a3c044ee4bdd7569f
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-29 21:04:09 -0500

    se suben las images al bucket S3 al momento de crear la categoria y se elimina la imagen al darle delete

--- backend/src/main/java/coworking/digitalBooking/Entities/Category.java
+++ backend/src/main/java/coworking/digitalBooking/Entities/Category.java
@@ -33,4 +33,35 @@ public class Category {
                 ", Description = " + description ;
     }
 
+    public Integer getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(Integer idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
