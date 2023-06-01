bb81a2ba2fb963e0805e40feb08555f9de34a6a0
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-05-31 23:19:39 -0500

    se modifica constants las urls

--- frontend/src/services/constants.js
+++ frontend/src/services/constants.js
@@ -1,9 +1,9 @@
 const root = 'http://3.141.149.37'
 
 export const constants = {
-    REGISTER_PRODUCTS_ENDPOINT: root + '/Products/register',
-    DELETE_PRODUCTS_ENDPOINT: root + '/Products/',
-    ALL_PRODUCTS_ENDPOINT: root + '/Products/all',
-    REGISTER_CATEGORIES_ENDPOINT: root + '/Categories/register',
-    ALL_CATEGORIES_ENDPOINT: root + '/Categories/all'
+    REGISTER_PRODUCTS_ENDPOINT: root + '/api/Products',
+    DELETE_PRODUCTS_ENDPOINT: root + '/api/Products',
+    ALL_PRODUCTS_ENDPOINT: root + '/api/Products',
+    REGISTER_CATEGORIES_ENDPOINT: root + '/api/Categories',
+    ALL_CATEGORIES_ENDPOINT: root + '/api/Categories'
 }
