fb601a14816541e96a96573e39c084a2a43d3972
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-06-01 21:51:55 -0500

    se actualiza contraseña

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,5 +1,5 @@
 export const authorizationUser = () => {
     //El usuario y la contrasena se deben concatenar con dos puntos y generar el base64 para
     // la autenticacion de las apis
-    return btoa('davidgalvis05@hotmail.com:Cow05*')
+    return btoa('davidgalvis05@hotmail.com:Elganador05*')
 }
