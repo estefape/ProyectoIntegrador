ec8c844d89f9bc4f4961db865a2645410a19e2d7
Estefania Mingochea <estefape06@gmail.com>
2023-06-07 03:25:23 -0300

    Revertir cambios en la IP de la API

--- frontend/src/services/constants.js
+++ frontend/src/services/constants.js
@@ -1,11 +1,11 @@
-const root = 'http://localhost:8080/api'
+const root = 'http://3.141.149.37/api'
 
 export const constants = {
     PRODUCTS_ENDPOINT: `${root}/Products/`,
     CATEGORIES_ENDPOINT: `${root}/Categories/`,
-    LOGIN_ENDPOINT: `${ root }/auth/login`,
-    REGISTER_ENDPOINT: `${ root }/auth/register`,
-    RATING_ENDPOINT: `${ root }/Ratings`,
+    LOGIN_ENDPOINT: `${root}/auth/login`,
+    REGISTER_ENDPOINT: `${root}/auth/register`,
+    RATING_ENDPOINT: `${root}/Ratings`,
     AUTH_HEADERS: {
         'Authorization': 'Basic ' + btoa('davidgalvis05@hotmail.com:Cow05*'),
         'Content-Type': 'application/json',
