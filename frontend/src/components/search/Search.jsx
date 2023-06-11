d1c7ffbefd37e91c840842eace4dc46b36f535d3
carodr <carodorado.r@gmail.com>
2023-06-10 19:38:06 -0500

    Se crea el bloque de buscar productos por ciudad

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -1,51 +1,86 @@
-import "./search.css"
+import "./search.css";
 import * as cityService from "../../services/cityServices";
-import React, {useEffect, useState } from "react";
-import useForm from "../../hooks/useForm";
-import { Calendar } from 'primereact/calendar';
-
-import 'primereact/resources/primereact.css';                       // core css
-import 'primeicons/primeicons.css';                                 // icons
-import 'primeflex/primeflex.css';  
-import 'primereact/resources/themes/saga-orange/theme.css'
-import './search.css'
+import * as productService from "../../services/productServices";
+import React, { useEffect, useState } from "react";
+import { Calendar } from "primereact/calendar";
+import { Dropdown } from "primereact/dropdown";
+import "primereact/resources/primereact.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "primereact/resources/themes/saga-orange/theme.css";
+import "./search.css";
 
 export const Search = () => {
-
-  const [ { city }, handleInputChanges, handleFileChanges, reset,] = useForm({ city: "", });
   const [cities, setCities] = useState([]);
   const [dates, setDates] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [productsFilter, setProductsFilter] = useState([products]);
+  const [selectedCity, setSelectedCity] = useState(null);
 
   useEffect(() => {
-      cityService
+    cityService
       .cityAll()
       .then((response) => {
         return response.json();
       })
       .then((cities) => {
         setCities(cities);
-      });   
+      });
+
+    productService
+      .productAll()
+      .then((response) => {
+        return response.json();
+      })
+      .then((productList) => {
+        setProducts(productList);
+      });
   }, []);
+
+  const filterProductsByCity = (product) => {
+    return product.city?.name == selectedCity.name;
+  };
+  
+  const handleSearch = () => {
+    setProductsFilter(products.filter(filterProductsByCity));
+  };
 
   return (
     <div className="search">
       <h2>Busca ofertas en oficinas...</h2>
       <div className="search-container">
-        <select name="city" value={city} onChange={handleInputChanges}>
-          <option value="">Seleccione una ciudad</option>
-            {cities.map((cit) => {
-              return (
-                <option key={cit.idCity} value={cit.idCity}>
-                  {cit.name}
-                </option>
-              );
-            })}
-        </select> 
-        
-            <Calendar id="calendar-picker" value={dates} onChange={(e) => setDates(e.value)} selectionMode="range" readOnlyInput />
-
-        <button className="btn">Buscar</button>
+        <Dropdown
+          value={selectedCity}
+          onChange={(e) => setSelectedCity(e.value)}
+          options={cities}
+          optionLabel="name"
+          placeholder="Seleccione una Ciudad"
+          filter
+        />
+        <Calendar
+          placeholder="Seleccione fechas disponibles"
+          id="calendar-picker"
+          value={dates}
+          onChange={(e) => setDates(e.value)}
+          selectionMode="range"
+          readOnlyInput
+        />
+        <button className="btn" onClick={handleSearch}>
+          Buscar
+        </button>
+      </div>
+      {/* El estado productsFilter contiene la lista de productos filtrados segun los criterios
+      de busqueda, reemplazar este bloque por el renderizado de los productos mostrados con cards
+      en la seccion home */}
+      <div>
+        {productsFilter.map((prod) => {
+          return (
+            <p className="co-filter" key={prod.name}>
+              {prod.name}
+            </p>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
